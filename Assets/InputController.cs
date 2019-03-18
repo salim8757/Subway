d6aa4574362368e397dcf4cf5ce450260ecd8699
salim8757 <36586600+salim8757@users.noreply.github.com>
2019-03-18 14:49:01 +0400

    Step1MoveSpawn

---
+++ Assets/InputController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+
+    private float jump = 5;
+    private float Speed = 5;
+    public bool isFalling;
+    Rigidbody playerRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetButtonDown("Play"))
+        transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position = new Vector3(transform.position.x
+                + 5 * Time.deltaTime, transform.position.y, transform.position.z);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position = new Vector3(transform.position.x
+                - 5 * Time.deltaTime, transform.position.y, transform.position.z);
+
+        if (Input.GetKey(KeyCode.Space) && isFalling == false)
+        {
+            playerRigidbody.velocity = new Vector3(0f, jump, 0f);
+            isFalling = true;
+        }
+    }
+
+
+    //to make sure you can't jump while in mid-air I use the following:
+    void OnCollisionStay()
+    {
+        isFalling = false;
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Coin;
+
+    float spwanTimer = 1;
+
+    List<GameObject> allCoins;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spwanTimer -= Time.deltaTime;
+        if (spwanTimer <= 0)
+        {
+            spwanTimer = 1;
+            allCoins.Add(Instantiate(Coin, new Vector3(Random.Range(-0.9f, 0.9f), 0.5f, Random.Range(-0.9f, 0.9f)),
+                Coin.transform.rotation));
+        }
+    }
+}
