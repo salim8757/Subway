1ae61a56b525a0cb351cc8784dce3187f081e73f
Ahlamalbalushi <47949009+Ahlamalbalushi@users.noreply.github.com>
2019-03-18 13:49:49 +0400

    spead

---
+++ Assets/scripts/increaseSpeedScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class increaseSpeedScript : MonoBehaviour
+{
+
+     float accelerationTime = 60;
+     float minSpeed;
+     float time;
+    float currentSpeed = 0f;
+    float maxSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        minSpeed = currentSpeed;
+        time = 0;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        increaseSpead();
+        
+    }
+
+
+    void increaseSpead()
+    {
+
+        currentSpeed = Mathf.SmoothStep(minSpeed, maxSpeed, time / accelerationTime);
+        time += Time.deltaTime;
+        print("My speed:" + currentSpeed);
+    }
+}
