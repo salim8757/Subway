03df04f2238daff27aa270148cbe5a12e83a6b76
Ahlamalbalushi <47949009+Ahlamalbalushi@users.noreply.github.com>
2019-03-18 13:41:05 +0400

    Read&write

---
+++ Assets/scripts/ReadWriteScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadWriteScript : MonoBehaviour
+{
+
+
+    string key = "PlayerScore";
+    float maxScore = 70;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SaveMaxScore();
+
+        getMaxScore();
+
+    }
+
+    void SaveMaxScore()
+    {
+        PlayerPrefs.SetFloat(key, maxScore);
+    }
+
+    void getMaxScore()
+    {
+        Debug.Log(PlayerPrefs.GetFloat(key).ToString());
+    }
+
+    void resetScore()
+    {
+        PlayerPrefs.DeleteKey(key);
+        
+    }
+
+ 
+
+
+    // Update is called once per frame
+
+}
