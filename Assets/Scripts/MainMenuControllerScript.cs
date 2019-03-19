44647044b9026bcb669ea0f2aff862ba037cb53f
RoaaAli <47963199+RoaaAli@users.noreply.github.com>
2019-03-19 13:38:03 +0400

    Activate Play Button

--- Assets/Scripts/MainMenuControllerScript.cs
+++ Assets/Scripts/MainMenuControllerScript.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuControllerScript : MonoBehaviour
 {
+
+    public Button playButton, saveSettingButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playButton.onClick.AddListener(SwitchScene);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void SwitchScene()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 }
