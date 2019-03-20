b8dfb3e466f6b879ba99e64b985c628f4cc0bb9f
RoaaAli <47963199+RoaaAli@users.noreply.github.com>
2019-03-20 13:51:32 +0400

    Instantiate Plane

---
+++ Assets/Scripts/MoveGround.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MoveGround : MonoBehaviour
+
+{
+    float OriginalPos = 35f;
+    private float Speed = 50;
+
+    // Start is called before the first frame update
+
+    void Start()
+
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+
+    {
+
+        transform.Translate(0, 0, Time.deltaTime * -Speed);
+
+        if (transform.position.z <= -66)
+
+        {
+
+            if (gameObject.tag == "plane")
+
+            {
+
+                transform.position = new Vector3(transform.position.x, transform.position.y, OriginalPos);
+
+            }
+
+
+
+            if (gameObject.tag == "coin")
+
+            {
+
+                Destroy(gameObject);
+
+                print("coin Destroyed");
+
+            }
+
+        }
+
+    }
+
+}
+
