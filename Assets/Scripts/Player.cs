e61ac61132c46586bd300e711f84bc47c77ad19f
RoaaAli <47963199+RoaaAli@users.noreply.github.com>
2019-03-20 09:33:47 +0400

    Update onCollisionEnter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public Text GameOver;
+    public float score; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameOver.enabled = false;
+        PlayerController.Instance.MyPlayer = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(1, 0, 0);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Stone")
+        {
+            GameOver.enabled = true;
+            Destroy(gameObject);
+            // Record the score 
+
+        }
+
+    }
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController Instance;
+
+    public Player MyPlayer { get; set; }
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //print(MyPlayer.Health);
     }
+
+    public void MoveForward()
+    {
+        MyPlayer.transform.Translate(1, 0, 0);
+    }
+
 }
