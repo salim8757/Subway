c2b2da2c698d437b9cf81cbd96cec97f7a5f86c4
RoaaAli <47963199+RoaaAli@users.noreply.github.com>
2019-03-20 10:48:40 +0400

    few updates in main menu scene

--- Assets/Scripts/MainMenuControllerScript.cs
+++ Assets/Scripts/MainMenuControllerScript.cs
@@ -8,12 +8,15 @@ public class MainMenuControllerScript : MonoBehaviour
 {
 
     public Button playButton;
-    public Text score, maxScore;
+    public Text Score, HighestScore;
+    public int GameCount = 0; 
 
     // Start is called before the first frame update
     void Start()
     {
         playButton.onClick.AddListener(SwitchScene);
+        HighestScore.enabled = false;
+        Score.enabled = false;
     }
 
     // Update is called once per frame
@@ -21,10 +24,17 @@ public class MainMenuControllerScript : MonoBehaviour
     {
         // Set the text's score
         //  Set the text's highest score
+
+        if (GameCount != 0)
+        {
+            HighestScore.enabled = true;
+            Score.enabled = true;
+        }
     }
 
     void SwitchScene()
     {
+        GameCount++;
         SceneManager.LoadScene("GameScene");
     }
 
