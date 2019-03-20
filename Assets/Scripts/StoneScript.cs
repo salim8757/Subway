fbda52d733df90a8fcf41ae9f27e769f3c8d0b7e
RoaaAli <47963199+RoaaAli@users.noreply.github.com>
2019-03-20 14:28:44 +0400

    Add Stone

---
+++ Assets/Scripts/StoneScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class StoneScript : MonoBehaviour
+
+{
+
+
+    [SerializeField]
+    GameObject Stone;
+    Transform myTransform;
+    float spwanTimer = 2;
+
+    // Start is called before the first frame update
+
+    void Start()
+
+    {
+
+        myTransform = GetComponent<Transform>();
+
+    }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+
+    {
+
+        spwanTimer -= Time.deltaTime;
+
+        if (spwanTimer <= 0)
+
+        {
+
+            spwanTimer = 2;
+
+
+
+            // float RandX = Random.Range(61f, 63f);
+
+            float RandX = Random.Range(-1f, 1f);
+
+            float RandZ = Random.Range(43.04f, 153.74f);
+
+            //float RandY = Random.Range(1f, 5f);
+
+
+
+            Vector3 StonePos = new Vector3(RandX, 0.2f, myTransform.transform.position.z + 3);
+
+            //Vector3 CoinPos = new Vector3(RandX, RandY, RandZ);
+
+
+
+            Instantiate(Stone, StonePos, Stone.transform.rotation);
+
+        }
+
+    }
+
+
+
+
+
+}
